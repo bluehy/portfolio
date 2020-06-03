fa2f62c3e725d7325e748ff3b540321c34f841cf
hy <chessg55@gmail.com>
2020-06-03 17:48:45 +0900

    load_seperated_files

--- js/src/main.js
+++ js/src/main.js
@@ -4,6 +4,11 @@
 const body = $('body');
 const wrap = body.children('#wrap');
 const headBox = wrap.children('#headBox');
+const viewBox = wrap.children('#viewBox');
+const conBox = wrap.children('#conBox');
+   const profileArea = conBox.children('.profile');
+   const workArea = conBox.children('.work');
+   const othersArea = conBox.children('.others');
 const footBox = wrap.children('#footBox');
 
 
@@ -17,6 +22,24 @@ const footBox = wrap.children('#footBox');
    headBox.load(htmlUrl+'headBox.html',function(){
       body.append(tempS+'headBox'+tempE);
    });
+
+   viewBox.load(htmlUrl+'viewBox.html',function(){
+      body.append(tempS+'viewBox'+tempE);
+   });
+
+   // conBox
+   profileArea.load(htmlUrl+'con_profile.html',function(){
+      body.append(tempS+'con_profile'+tempE);
+   });
+
+   workArea.load(htmlUrl+'con_work.html',function(){
+      body.append(tempS+'con_work'+tempE);
+   });
+
+   othersArea.load(htmlUrl+'con_others.html',function(){
+      body.append(tempS+'con_others'+tempE);
+   });
+
 
    footBox.load(htmlUrl+'footBox.html',function(){
       body.append(tempS+'footBox'+tempE);
